4e372d78cba55d61fbdf9238671f45ea71069089
Wanyu Zhao <wanyu0214@gwmail.gwu.edu>
2025-04-10 00:14:18 -0400

    Save my local changes before pulling

--- examples/NEP/graphite/input/run_lammps.sh
+++ examples/NEP/graphite/input/run_lammps.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-#PBS -A 
-#PBS -q 
-#PBS -l select=4:ncpus=128:mpiprocs=128
-#PBS -l walltime=24:00:00
-#PBS -j oe
-#PBS -N 3900-15
-
 
 
 aprun -n 512 path/src/lmp_mpi -in lammps.input -screen none -ffs 128 ffs.input 
--- examples/mW/input/run_lammps.sh
+++ examples/mW/input/run_lammps.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-#SBATCH -A 
-#SBATCH -p 
-#SBATCH --nodes=4
-#SBATCH --ntasks=512
-#SBATCH --time=3:00:00
-#SBATCH --job-name test
-#
 
 
 mpirun -np 512 path/lmp_mpi -in lammps.input -screen none -ffs 128 ffs.input
